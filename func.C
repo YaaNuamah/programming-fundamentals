be57407ccf6ba493d1370e79d1c13a5ed0425638
Yaa <yaa_fordjour@hotmail.com>
2019-10-22 18:43:59 +0100

    first commit

---
+++ func.C
@@ -0,0 +1,19 @@
+#include <iostream>
+int getnum()
+ {
+ std::cout << "Enter a number";
+ std::cin  >>auto num1;
+
+ return num1;
+}
+int main()
+{
+  int num1;
+  int result;	
+
+getnum(num1);
+getnum(num1);
+ 
+cout <<
+
+}
---
+++ mult.C
@@ -0,0 +1,15 @@
+#include <iostream>
+int main()
+{
+  int x, y, result;
+  std::cout << "Enter two numbers";
+  std::cin >> x >> y;
+  result = x * y;
+if(result > 100)
+{
+  std::cout << "The result is HIGH ";
+ } 
+  else {
+    std::cout << "The result is LOW "; 
+}
+}
